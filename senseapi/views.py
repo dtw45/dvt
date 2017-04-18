9f999be03164383e2050c7f1920bd373a8215321
Derek Wladyka <derek.wladyka@gmail.com>
2017-04-18 17:36:52 -0400

    started using marshmallow

--- senseapi/views.py
+++ senseapi/views.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify, request
 from flask.views import MethodView
+from flask_api import status
 from senseapi import app, db
+from senseapi.models import Accelerometer
 from senseapi.schemas import AccelerometerSchema
 
 class AccelerometerListView(MethodView):
@@ -8,16 +10,31 @@ class AccelerometerListView(MethodView):
     schema = AccelerometerSchema()
 
     def get(self):
-        #TODO: return accelerometer data
-        return ""
+
+        data = Accelerometer.query.all()
+
+        response = self.schema.dump(data, many=True).data
+
+        return jsonify(response)
 
     def post(self):
 
         # Deserialize the data and queue to be commited
-        for data in request.json:
-            instance = self.schema.make_instance(data)
-            db.session.add(instance)
+        for d in request.json:
+            instance, errors = self.schema.load(d)
 
-        db.session.commit()
+            if errors:
+                return jsonify({'errors': errors}), 422
 
-        return "200 OK"
+            import ipdb; ipdb.set_trace()
+            instance.save()
+        #     db.session.add(instance)
+        #
+        # db.session.commit()
+
+        # response = []
+        #
+        # for data in request.json:
+        #     response.append(self.schema.load(data).data)
+
+        return jsonify(instance)
